afa7c4fa2fe20b164cc12f2d24631e065ca480f9
Wiktor Nobis <nobis171@wp.pl>
2022-07-01 13:34:05 +0200

    first step to-do list

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,25 @@
-console.log('Hello123');
+// <li>
+// <label for="task-1">Wyrzucić śmieci</label>
+// <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+// </li>
+const taskNameInputElement = document.querySelector('#name');
+const addButtonElement = document.querySelector('button');
+const tasksContainerElement = HTMLElement = document.querySelector('.tasks');
+const tasks = ['Wyrzucić śmieci', 'Pójść na siłownię', 'Nakarmić koty'];
+const render = () => {
+    tasksContainerElement.innerHTML = '';
+    tasks.forEach(task => {
+        const tasksElement = document.createElement('li');
+        tasksElement.innerText = task;
+        tasksContainerElement.appendChild(tasksElement);
+    });
+};
+const addTask = (task) => {
+    tasks.push(task);
+};
+addButtonElement.addEventListener('click', (event) => {
+    event.preventDefault();
+    tasks.push(taskNameInputElement.value);
+    render();
+});
+render();
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log('Hello123')
+// <li>
+// <label for="task-1">Wyrzucić śmieci</label>
+// <input type="checkbox" id="task-1" name="Wyrzucić śmieci" />
+// </li>
+const taskNameInputElement: HTMLInputElement = document.querySelector('#name');
+const addButtonElement: HTMLButtonElement = document.querySelector('button');
+
+const tasksContainerElement = HTMLElement = document.querySelector('.tasks');
+
+const tasks: string[] = ['Wyrzucić śmieci', 'Pójść na siłownię', 'Nakarmić koty'];
+
+const render = () => {
+    tasksContainerElement.innerHTML = ''
+    tasks.forEach(task => {
+        const tasksElement: HTMLElement = document.createElement('li')
+        tasksElement.innerText = task;
+        tasksContainerElement.appendChild(tasksElement);
+    })
+}
+
+const addTask = (task: string) => {
+    tasks.push(task)
+}
+
+addButtonElement.addEventListener('click', (event: Event) => {
+    event.preventDefault();
+    tasks.push(taskNameInputElement.value)
+    render();
+
+})
+
+render();
+
